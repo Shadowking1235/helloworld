f29c2c42cdf95f026f037f361cc9e5d71c48a480
aakashjha001 <46425065+aakashjha001@users.noreply.github.com>
2019-01-08 21:00:46 +0500

    Initial

---
+++ homework2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 08 13:07:35 2019
+
+@author: Aakash Jha
+"""
+
+Artist = "Shawn Mendes"
+Genre = "Pop"
+Year = 1998
+
+
+def artist():
+    print(Artist)
+
+def genre():
+    print(Genre)
+
+def year():
+    print(Year) 
+
+ 
+    
+artist()
+genre()
+year()
+
