4d8223c97d7b543de9b1fd5fb214a74f97e25107
aakashjha001 <46425065+aakashjha001@users.noreply.github.com>
2019-01-08 10:15:54 +0500

    Initial

---
+++ main.py
@@ -0,0 +1,23 @@
+#Attributes to the song
+
+Genre="Pop"
+Artist="Shawn Mendes"
+'''
+B denotes billion views on youtube
+ Spotify denotes the song ranked on spotify
+ Duration of the song is given in minutes
+ Copies sold variable denotes the copies of the song sold
+ '''
+Views="1.6B"
+Duration=3.52
+SpotifyRank=3
+Year=2017
+CopiesSold=300000
+
+print(Genre)
+print(Artist)
+print(Views)
+print(Duration)
+print(SpotifyRank)
+print(Year)
+print(CopiesSold)
